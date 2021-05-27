2bbbe22040f3cf0f298d57bec8236385ed3cc581
ruiqingluo <ruiqingluo1@163.com>
2021-05-27 09:48:08 +0800

    # update

--- src/leetcode/hot100/Solution003.py
+++ src/leetcode/hot100/Solution003.py
@@ -1,3 +1,20 @@
 def length_of_longest_substring(s: str) -> int:
+    # hash set, record if a letter occurred
+    occ = set()
+    n = len(s)
+    # left pointer-left, right pointer - right
+    ans, right = 0, -1
+    for i in range(n):
+        if i != 0:
+            # left pointer right move, remove the letter which left pointer pointing from occ
+            occ.remove(s[i - 1])
+        while right + 1 < n and s[right + 1] not in occ:
+            occ.add(s[right + 1])
+            right += 1
 
-    return 0
+        ans = max(ans, right - i + 1)
+
+    return ans
+
+
+print(length_of_longest_substring(''))
